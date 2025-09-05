f113a80661dbdea18506ff13abea7e237d0dada0
LukeDurham <durham35@marshall.edu>
2025-09-05 09:53:22 -0400

    add supabase and companion creation with supporting auth channels

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,6 +1,10 @@
 import CompanionForm from "@/components/companionform";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-const NewCompanion = () => {
+const NewCompanion = async () => {
+  const { userId } = await auth();
+  if (!userId) redirect("/sign-in");
   return (
     <main className="min-lg:w-1/3 mind-md:w-2/3 items-center justify-center">
       <article className="w-full gap-4 flex flex-col">
