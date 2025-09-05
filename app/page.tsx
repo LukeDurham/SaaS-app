4542edddc878457285d1241109f65388e8a4fad9
LukeDurham <durham35@marshall.edu>
2025-09-05 15:48:43 -0400

    add companion get user sessions in server actions

--- app/page.tsx
+++ app/page.tsx
@@ -4,43 +4,33 @@ import CompanionCard from "@/components/companioncard";
 import CompanionsList from "@/components/companionslist";
 import CTA from "@/components/cta";
 import { recentSessions } from "@/constants";
+import {
+  getAllCompanions,
+  getRecentSessions,
+} from "@/lib/actions/companion.actions";
+import { getSubjectColor } from "@/lib/utils";
 
-const Home = () => {
+const Home = async () => {
+  const companions = await getAllCompanions({ limit: 3 });
+  const recentSessionsCompanions = await getRecentSessions(10);
   return (
     <div>
       <h1 className="text-2xl underline">Welcome to SaaS App</h1>
 
       <section className="home-section">
-        <CompanionCard
-          id="123"
-          name="Neura the Brainy Explorer"
-          topic="Neural Network of the Brain"
-          subject="science"
-          duration={45}
-          color="#ffda6e"
-        />
-        <CompanionCard
-          id="456"
-          name="Countsy the Number Wizard"
-          topic="Derivatives & Integrals"
-          subject="Maths"
-          duration={30}
-          color="#e5d0ff"
-        />
-        <CompanionCard
-          id="789"
-          name="Verba the Vocabulary Builder"
-          topic="English Literature"
-          subject="Language"
-          duration={30}
-          color="#177bb4"
-        />
+        {companions.map((companion) => (
+          <CompanionCard
+            key={companion.id}
+            {...companion}
+            color={getSubjectColor(companion.subject)}
+          />
+        ))}
       </section>
 
       <section className="home-section">
         <CompanionsList
           title="recently completed sessions"
-          companions={recentSessions}
+          companions={recentSessionsCompanions}
           classNames="w-2/3 max-lg:w-full"
         />
         <CTA />
