ed8c471a6fc426d28ce3f0872cb7ab6dbc33ebe2
LukeDurham <durham35@marshall.edu>
2025-09-15 16:52:47 -0400

    fix build error

--- lib/actions/companion.actions.ts
+++ lib/actions/companion.actions.ts
@@ -5,18 +5,22 @@ import { createSupabaseClient } from "@/lib/supabase";
 import { revalidatePath } from "next/cache";
 
 export const createCompanion = async (formData: CreateCompanion) => {
-  const { userId: author } = await auth();
-  const supabase = createSupabaseClient();
+  try {
+    const { userId: author } = await auth();
+    const supabase = createSupabaseClient();
 
-  const { data, error } = await supabase
-    .from("companions")
-    .insert({ ...formData, author })
-    .select();
+    const { data, error } = await supabase
+      .from("companions")
+      .insert({ ...formData, author })
+      .select();
 
-  if (error || !data)
-    throw new Error(error?.message || "Failed to create a companion");
+    if (error || !data)
+      throw new Error(error?.message || "Failed to create a companion");
 
-  return data[0];
+    return data[0];
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export const getAllCompanions = async ({
